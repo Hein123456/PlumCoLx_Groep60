c63da9648136d3612705d3a65b6d683134e65813
Hein123456 <heinrichenslin91@gmail.com>
2023-09-02 17:37:28 +0200

    user form 1

---
+++ PlumCoLx_Groep60/User.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace PlumCoLx_Groep60
+{
+    partial class User
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.navToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.vanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.navToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.usernameToolStripMenuItem = new System.Windows.Forms.ToolStripTextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.navToolStripMenuItem,
+            this.vanToolStripMenuItem,
+            this.navToolStripMenuItem1,
+            this.usernameToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(0);
+            this.menuStrip1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.menuStrip1.Size = new System.Drawing.Size(1229, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // navToolStripMenuItem
+            // 
+            this.navToolStripMenuItem.Name = "navToolStripMenuItem";
+            this.navToolStripMenuItem.Size = new System.Drawing.Size(49, 27);
+            this.navToolStripMenuItem.Text = "Info";
+            this.navToolStripMenuItem.Click += new System.EventHandler(this.navToolStripMenuItem_Click);
+            // 
+            // vanToolStripMenuItem
+            // 
+            this.vanToolStripMenuItem.Name = "vanToolStripMenuItem";
+            this.vanToolStripMenuItem.Size = new System.Drawing.Size(46, 27);
+            this.vanToolStripMenuItem.Text = "van";
+            // 
+            // navToolStripMenuItem1
+            // 
+            this.navToolStripMenuItem1.Name = "navToolStripMenuItem1";
+            this.navToolStripMenuItem1.Size = new System.Drawing.Size(46, 27);
+            this.navToolStripMenuItem1.Text = "nav";
+            // 
+            // usernameToolStripMenuItem
+            // 
+            this.usernameToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.usernameToolStripMenuItem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(19)))), ((int)(((byte)(18)))));
+            this.usernameToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.usernameToolStripMenuItem.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(7)))), ((int)(((byte)(10)))), ((int)(((byte)(82)))));
+            this.usernameToolStripMenuItem.Name = "usernameToolStripMenuItem";
+            this.usernameToolStripMenuItem.ReadOnly = true;
+            this.usernameToolStripMenuItem.Size = new System.Drawing.Size(87, 27);
+            this.usernameToolStripMenuItem.Text = "username";
+            // 
+            // User
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(19)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(1229, 640);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "User";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "InfoUser";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.User_FormClosing);
+            this.Load += new System.EventHandler(this.User_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem navToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem vanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem navToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripTextBox usernameToolStripMenuItem;
+    }
+}
---
+++ PlumCoLx_Groep60/UserInfo.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PlumCoLx_Groep60
+{
+    partial class UserInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlBack = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // pnlBack
+            // 
+            this.pnlBack.BackColor = System.Drawing.Color.Black;
+            this.pnlBack.Location = new System.Drawing.Point(-8, -8);
+            this.pnlBack.Name = "pnlBack";
+            this.pnlBack.Padding = new System.Windows.Forms.Padding(5);
+            this.pnlBack.Size = new System.Drawing.Size(1326, 672);
+            this.pnlBack.TabIndex = 0;
+            // 
+            // UserInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(19)))), ((int)(((byte)(18)))));
+            this.ClientSize = new System.Drawing.Size(1406, 726);
+            this.Controls.Add(this.pnlBack);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "UserInfo";
+            this.Text = "UserInfo";
+            this.Load += new System.EventHandler(this.UserInfo_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBack;
+    }
+}
