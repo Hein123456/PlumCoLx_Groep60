c7847ac0cdc88f2565560284fa3de4840da00bbe
Hein123456 <heinrichenslin91@gmail.com>
2023-09-02 11:50:03 +0200

    login page

--- PlumCoLx_Groep60/Form1.cs
+++ PlumCoLx_Groep60/Form1.cs
@@ -16,5 +16,10 @@ namespace PlumCoLx_Groep60
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
