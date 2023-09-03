503e41da3ca7218adaa258eb38afd0cb3a31e507
Hein123456 <heinrichenslin91@gmail.com>
2023-09-04 00:30:38 +0200

    track services fin

--- PlumCoLx_Groep60/UserServiceTracking.cs
+++ PlumCoLx_Groep60/UserServiceTracking.cs
@@ -29,30 +29,96 @@ namespace PlumCoLx_Groep60
         private void UserServiceTracking_Load(object sender, EventArgs e)
         {
             // display all service requests from the Jobs tabel and the Joblog table where the client id is the same as the logged in user in the datagridview
-            con.Open();
-            String query = "select * from Jobs where ClientID = '" + userid + "'";
-            cmd = new SqlCommand(query, con);
-            adapt = new SqlDataAdapter(cmd);
-            adapt.SelectCommand.ExecuteNonQuery();
-            DataSet ds = new DataSet();
-            adapt.Fill(ds, "jobs");
-            dataGridView1.DataSource = ds;
-            dataGridView1.DataMember = "jobs";
-            con.Close();
-            String query2 = "select * from Joblog where ClientID = '" + userid + "'";
-            con.Close();
-            con.Open();
-            cmd = new SqlCommand(query2, con);
-            adapt = new SqlDataAdapter(cmd);
-            adapt.SelectCommand.ExecuteNonQuery();
-            
+            try
+            {
+                con.Open();
+                String query = "select * from Jobs where ClientID = '" + userid + "'";
+                cmd = new SqlCommand(query, con);
+                adapt = new SqlDataAdapter(cmd);
+                adapt.SelectCommand.ExecuteNonQuery();
+                DataSet ds = new DataSet();
                 adapt.Fill(ds, "jobs");
-            dataGridView1.DataSource = ds;
-            dataGridView1.DataMember = "jobs";
-            con.Close();
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "jobs";
+                con.Close();
+                String query2 = "select * from Joblog where ClientID = '" + userid + "'";
+                con.Close();
+                con.Open();
+                cmd = new SqlCommand(query2, con);
+                adapt = new SqlDataAdapter(cmd);
+                adapt.SelectCommand.ExecuteNonQuery();
+
+                adapt.Fill(ds, "jobs");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "jobs";
+                con.Close();
+            } catch (Exception ex) { MessageBox.Show(ex.Message); }
 
 
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                String query = "select * from Jobs where ClientID = '" + userid + "'";
+                cmd = new SqlCommand(query, con);
+                adapt = new SqlDataAdapter(cmd);
+                adapt.SelectCommand.ExecuteNonQuery();
+                DataSet ds = new DataSet();
+                adapt.Fill(ds, "jobs");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "jobs";
+                con.Close();
+                String query2 = "select * from Joblog where ClientID = '" + userid + "'";
+                con.Close();
+                con.Open();
+                cmd = new SqlCommand(query2, con);
+                adapt = new SqlDataAdapter(cmd);
+                adapt.SelectCommand.ExecuteNonQuery();
+
+                adapt.Fill(ds, "jobs");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "jobs";
+                con.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                //show only pending service requests
+                String query = "select * from Jobs where ClientID = '" + userid + "' and Status = 'Pending'";
+              
+                cmd = new SqlCommand(query, con);
+                adapt = new SqlDataAdapter(cmd);
+                adapt.SelectCommand.ExecuteNonQuery();
+                DataSet ds = new DataSet();
+                adapt.Fill(ds, "jobs");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "jobs";
+                con.Close();
+                //show only pending service requests'
+                String query2 = "select * from Joblog where ClientID = '" + userid + "' and Status = 'Pending'";
+                
+                con.Close();
+                con.Open();
+                cmd = new SqlCommand(query2, con);
+                adapt = new SqlDataAdapter(cmd);
+                adapt.SelectCommand.ExecuteNonQuery();
+
+                adapt.Fill(ds, "jobs");
+                dataGridView1.DataSource = ds;
+                dataGridView1.DataMember = "jobs";
+                con.Close();
+            }
+            catch (Exception ex) { MessageBox.Show(ex.Message); }
+        }
+    }
     }
 }
