9ac14879e7c4ad1c57e3859a8f340100a11c7219
Hein123456 <heinrichenslin91@gmail.com>
2023-09-03 16:23:45 +0200

    register fin 1

--- PlumCoLx_Groep60/Register.cs
+++ PlumCoLx_Groep60/Register.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,13 @@ namespace PlumCoLx_Groep60
 {
     public partial class Register : Form
     {
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd;
+        SqlDataAdapter adapt;
         public Register()
         {
             InitializeComponent();
+            con.ConnectionString = @"Data Source = (LocalDB)\MSSQLLocalDB; AttachDbFilename = |DataDirectory|\PlumCo.mdf; Integrated Security = True";
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -24,6 +30,52 @@ namespace PlumCoLx_Groep60
             String Password = textBox2.Text;
             String Address = textBox4.Text;
             String Phone = textBox3.Text;
+            // check if user exists
+            con.Open();
+            string sql = "SELECT * FROM ClientID WHERE name = '" + Username + "' AND Password = '" + Password + "'";
+            cmd = new SqlCommand(sql, con);
+            adapt = new SqlDataAdapter(cmd);
+            adapt.SelectCommand.ExecuteNonQuery();
+            DataSet ds = new DataSet();
+            adapt.Fill(ds);
+            int count = ds.Tables[0].Rows.Count;
+            // if user doesn't exist add user to database
+            if (count == 0)
+            {
+                //generate new integer id based on ids in table
+                string sql1 = "SELECT MAX(id) FROM ClientID";
+                cmd = new SqlCommand(sql1, con);
+                adapt = new SqlDataAdapter(cmd);
+                adapt.SelectCommand.ExecuteNonQuery();
+                DataSet ds1 = new DataSet();
+                adapt.Fill(ds1);
+                int id = Convert.ToInt32(ds1.Tables[0].Rows[0][0]) + 1;
+
+                
+                // add user to database
+                string sql2 = "INSERT INTO ClientID (ClientID, name, Password, Address, phone_Num) VALUES ("+id+"'" + Username + "', '" + Password + "', '" + Address + "', '" + Phone + "')";
+                cmd = new SqlCommand(sql2, con);
+                adapt = new SqlDataAdapter(cmd);
+                adapt.InsertCommand.ExecuteNonQuery();
+                // create text file with username
+                string path = "login.txt";
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(Username);
+                    sw.WriteLine(id);
+                    sw.WriteLine("user");
+                    sw.WriteLine(DateTime.Now);
+                }
+                // show user form
+                User user = new User();
+                user.Show();
+                this.Hide();
+            }
+            else
+            {
+                // if user exists show error message
+                MessageBox.Show("User already exists");
+            }   
 
         }
     }
