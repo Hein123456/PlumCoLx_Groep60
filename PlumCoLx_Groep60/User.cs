880334341f6997d535b0c5a4485aba996d24ff16
Hein123456 <heinrichenslin91@gmail.com>
2023-09-03 16:13:58 +0200

    switch user test 1

--- PlumCoLx_Groep60/User.cs
+++ PlumCoLx_Groep60/User.cs
@@ -121,7 +121,11 @@ namespace PlumCoLx_Groep60
         private void switchUserToolStripMenuItem_Click(object sender, EventArgs e)
         {
             // delete file
+            System.IO.File.Delete("login.txt");
             // show llogin
+            Login login = new Login();
+            login.Show();
+            this.Hide();
         }
     }
 }
