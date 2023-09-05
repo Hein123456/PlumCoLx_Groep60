591cc42922fa0e2d1fb9446d9e8de65abff0b105
Heinrich Enslin <37419935@mynwu.ac.za>
2023-09-05 02:31:34 +0200

    Fin

--- PlumCoLx_Groep60/UserFinancial.Designer.cs
+++ PlumCoLx_Groep60/UserFinancial.Designer.cs
@@ -39,9 +39,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(32, 488);
+            this.button3.Location = new System.Drawing.Point(24, 396);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(154, 55);
+            this.button3.Size = new System.Drawing.Size(116, 45);
             this.button3.TabIndex = 26;
             this.button3.Text = "Show Pending Transactions";
             this.button3.UseVisualStyleBackColor = true;
@@ -51,27 +52,30 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(193, 14);
+            this.label1.Location = new System.Drawing.Point(145, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(254, 39);
+            this.label1.Size = new System.Drawing.Size(203, 31);
             this.label1.TabIndex = 23;
             this.label1.Text = "User Financial: ";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(22, 68);
+            this.dataGridView1.Location = new System.Drawing.Point(16, 55);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(598, 373);
+            this.dataGridView1.Size = new System.Drawing.Size(448, 303);
             this.dataGridView1.TabIndex = 22;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(277, 488);
+            this.button1.Location = new System.Drawing.Point(208, 396);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(154, 36);
+            this.button1.Size = new System.Drawing.Size(116, 29);
             this.button1.TabIndex = 27;
             this.button1.Text = "Show all transactions";
             this.button1.UseVisualStyleBackColor = true;
@@ -79,25 +83,27 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(87, 581);
+            this.textBox1.Location = new System.Drawing.Point(65, 472);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(99, 22);
+            this.textBox1.Size = new System.Drawing.Size(75, 20);
             this.textBox1.TabIndex = 28;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(84, 562);
+            this.label2.Location = new System.Drawing.Point(63, 457);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(124, 20);
+            this.label2.Size = new System.Drawing.Size(80, 13);
             this.label2.TabIndex = 29;
             this.label2.Text = "Total ammount:";
             // 
             // UserFinancial
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(636, 638);
+            this.ClientSize = new System.Drawing.Size(477, 556);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -105,6 +111,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "UserFinancial";
             this.Text = "UserFinancial";
             this.Load += new System.EventHandler(this.UserFinancial_Load);
